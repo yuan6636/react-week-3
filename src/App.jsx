34bbff8aed8bcec25fbbb9e8310d87e4617eafab
yuan6636 <aleniverson777@gmail.com>
2026-01-13 20:43:52 +0800

    feat: 新增登入功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,81 @@
+import { useState } from "react";
+import axios from "axios";
+
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
 
 function App() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${API_BASE}/admin/signin`, formData);
+      const {token, expired} = res.data;
+      document.cookie = `access_token=${token}; expires=${new Date(expired)};`;
+    } catch (error) {
+      alert("登入失敗: " + error?.response?.data?.message);
+    }
+  };
 
   return (
     <>
+      <div className="container login">
+        <div className="row justify-content-center">
+          <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
+          <div className="col-8">
+            <form id="form" className="form-signin" onSubmit={handleSubmit}>
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="username"
+                  placeholder="name@example.com"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  required
+                  autoFocus
+                />
+                <label htmlFor="username">Email address</label>
+              </div>
+              <div className="form-floating">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  required
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+              <button
+                className="btn btn-lg btn-primary w-100 mt-3"
+                type="submit"
+              >
+                登入
+              </button>
+            </form>
+          </div>
+        </div>
+        <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
+import './assets/style/style.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
