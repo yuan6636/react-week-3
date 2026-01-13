db305d02568cc79710da658b507e09c84acf0c40
yuan6636 <aleniverson777@gmail.com>
2026-01-13 17:09:03 +0800

    專案初始化

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
